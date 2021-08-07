e5845662fc96a14e413bf7bb626e341817f07c6a
Preet <sd.preet27@gmail.com>
2021-08-07 11:37:32 +0800

    Project setup

---
+++ CraftDigital/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CraftDigital
+//
+//  Created by Simran on 07/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
